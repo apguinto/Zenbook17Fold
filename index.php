570e2aa32dd52bb93a5995d07fe8e364fd797eea
Avery Paula Guinto <91929688+apguinto@users.noreply.github.com>
2023-02-15 00:59:05 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php 
-    include "shared/head.php";
+include "shared/head.php";
 ?>
+
 <body>
     <?php 
-        include "shared/header.php";
+    include "shared/header.php";
     ?>
     
     <div id="first-section">
@@ -21,70 +22,31 @@
     <div id="second-section">
         <div id="details">Zenbook 17 Fold OLED leads the world with its amazing 17.3” Foldable OLED (FOLED) touchscreen1 that folds down instantly to a compact 12.5” size, making it smaller than a sheet of photocopier paper for easy carrying. The color-accurate 2.5K slim-bezel NanoEdge Dolby Vision screen is also PANTONE® Validated with TÜV Rheinland-certified low blue-light levels. Its ingenious multiple user modes are limited only by your imagination, and the detachable ASUS ErgoSense Bluetooth® keyboard and touchpad turns it into a powerful fully-featured laptop with a 12th Gen Intel® Core™ processor and a stunning Harman Kardon-certified quad-speaker Dolby Atmos® audio system. The innovative camera system includes a 5 MP AI webcam, a HD IR camera for fast face login, and a color sensor for automatic brightness and color temperature control.</div>
     </div>
+    
     <div id="third-section">
         <div class="section-title">A NEW HYBRID OF LAPTOP AND TABLET</div>
         <div class="line-separator"></div>
+        <?php
+            $query = "SELECT * FROM Asus_Fold";
+            $result = executeQuery($query);
+
+            while ($row = mysqli_fetch_assoc($result)) {
+        ?>
         <div class="container">
             <div class="col-sm-6 col-xs-12" id="laptop-mode-vk">
-                <p class="mode">Laptop Mode (with virtual keyboard)</p>
-                <p class="dimension">12.5” 3:2 Laptop</p>
-                <p class="description">Enjoy a similar viewing area to a 13.3” 16:9 laptop. Use the 12.5-inch laptop with the virtual onscreen keyboard for an immediate con-call.</p>
+                <p class="mode"><?php echo $row["mode"]."<br>"?></p>
+                <p class="dimension"><?php echo $row["dimension"] ?></p>
+                <p class="description"><?php echo $row["description"] ?></p>
             </div>
             <div class="col-sm-6 col-xs-12">
-                <img class="product-picture" src="https://dlcdnwebimgs.asus.com/gain/f4566c86-2a10-4c32-99f5-35918db38561/w800">
+                <img class="product-picture" src="<?php echo $row["image"] ?>">
             </div>
         </div>
-        <div class="container">
-            <div class="col-sm-6 col-xs-12">
-                <img class="product-picture" src="https://dlcdnwebimgs.asus.com/gain/fc1f404f-92aa-4fad-8f14-0f7a88ec9aae/w800">
-            </div>
-            <div class="col-sm-6 col-xs-12" id="laptop-mode-bk">
-                <p class="mode">Laptop Mode (with Bluetooth® keyboard)</p>
-                <p class="dimension">12.5” 3:2 Laptop</p>
-                <p class="description">Enjoy a similar viewing area to a 13.3” 16:9 laptop. Enhance the 12.5-inch laptop with the ASUS ErgoSense Bluetooth® keyboard and touchpad placed on the lower screen for web browsing.</p>
-            </div>
-        </div>
-        <div class="container">
-            <div class="col-sm-6 col-xs-12" id="desktop-mode">
-                <p class="mode">Desktop Mode</p>
-                <p class="dimension">17.3” 4:3 2-in1 PC</p>
-                <p class="description">For the serious stuff, unfold the foldable OLED display to its full 17.3-inch size, prop it up on your desk with the integrated kickstand and connect the ErgoSense Bluetooth® keyboard.</p>
-            </div>
-            <div class="col-sm-6 col-xs-12">
-                <img class="product-picture" src="https://dlcdnwebimgs.asus.com/gain/59e47fc2-ef03-42db-b421-06c9a406399e/w800">
-            </div>
-        </div>
-        <div class="container">
-            <div class="col-sm-6 col-xs-12">
-                <img class="product-picture" src="https://dlcdnwebimgs.asus.com/gain/f27439b7-464c-4248-87e7-9cc62649aeb1/w800">
-            </div>
-            <div class="col-sm-6 col-xs-12" id="tablet-mode">
-                <p class="mode">Tablet Mode</p>
-                <p class="dimension">17.3” 4:3 Pad</p>
-                <p class="description">Fully opened, the 17.3-inch touchscreen is perfect for presenting your big ideas, or simply kicking back to relax on the web.</p>
-            </div>
-        </div>
-        <div class="container">
-            <div class="col-sm-6 col-xs-12" id="reader-mode">
-                <p class="mode">Reader Mode</p>
-                <p class="dimension">17.3” 4:3 Folded E-book</p>
-                <p class="description">Fold it vertically and it's a beautifully clear reading device. Your eyes are protected with the TÜV Rheinland-certified low blue-light emissions.</p>
-            </div>
-            <div class="col-sm-6 col-xs-12">
-                <img class="product-picture" src="https://dlcdnwebimgs.asus.com/gain/331cebbb-5eb3-4f25-9bb8-cc161292b8a0/w800">
-            </div>
-        </div>
-        <div class="container">
-            <div class="col-sm-6 col-xs-12">
-                <img class="product-picture" src="https://dlcdnwebimgs.asus.com/gain/70516022-fe29-42e9-9497-ba4353fc1631/w800">
-            </div>
-            <div class="col-sm-6 col-xs-12" id="extended-mode">
-                <p class="mode">Extended Mode</p>
-                <p class="dimension">12.5” 3:2 Laptop with two 12.5” full-size displays</p>
-                <p class="description">Utilize the full portrait display with the wireless keyboard to let your imagination run wild.</p>
-            </div>
-        </div>
+        <?php
+            }
+        ?>
     </div>
+
     <div id="fourth-section">
         <div class="section-title" style="color: #000000;">Power the incredible</div>
         <div class="line-separator"></div>
